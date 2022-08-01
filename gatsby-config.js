6b9ac070e8b875d77d55486ec71fe3faba0204d9
HanamiYuushimo <591272415@qq.com>
2022-08-01 18:34:47 +0800

    Update

---
+++ gatsby-config.js
@@ -0,0 +1,72 @@
+module.exports = {
+  pathPrefix: '/running_page', // Change to `/running_page` when running on github pages
+  siteMetadata: {
+    siteTitle: 'My Running Page',
+    siteUrl: 'https://github.com/HanamiYuushimo/running_page',
+    logo: 'https://raw.githubusercontent.com/HanamiYuushimo/running_page/main/headicon.png',
+    description: 'Personal site and blog',
+    navLinks: [
+      {
+        name: 'My Github',
+        url: 'https://github.com/HanamiYuushimo',
+      },
+      {
+        name: 'About',
+        url: 'https://github.com/HanamiYuushimo/running_page/blob/main/README.md',
+      },
+    ],
+  },
+  plugins: [
+    'gatsby-transformer-json',
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        path: './src/static/',
+      },
+    },
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        path: './src/images/',
+      },
+    },
+    {
+      resolve: 'gatsby-alias-imports',
+      options: {
+        rootFolder: './',
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-sass',
+      options: {
+        cssLoaderOptions: {
+          esModule: false,
+          modules: {
+            namedExport: false,
+          },
+        },
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-react-svg',
+      options: {
+        rule: {
+          include: /assets/,
+        },
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-manifest',
+      options: {
+        name: 'gatsby-starter-default',
+        short_name: 'starter',
+        start_url: '/',
+        background_color: '#e1e1e1',
+        theme_color: '#e1e1e1',
+        display: 'minimal-ui',
+        icon: 'src/images/favicon.png', // This path is relative to the root of the site.
+      },
+    }
+  ],
+};
